92582e3f1f84337efb19c6dc7406272aa11297a1
abeRC <aeciobeltrao@usp.br>
2020-08-25 23:18:09 -0300

    um pouquinho melhor?

--- assets/js/navbar-autohide.js
+++ assets/js/navbar-autohide.js
@@ -1,11 +1,32 @@
-/*Quando o usuário rola para baixo, esconda a navbar. Quando rola para cima, msotre a navbar.*/
+/**
+ * Quando o usuário rola para baixo, esconde a navbar. Quando rola para cima, mostra a navbar.
+ * FIXME: https://developer.mozilla.org/docs/Mozilla/Performance/ScrollLinkedEffects
+ * */
+
 var prevScrollpos = window.pageYOffset; // inicializa a posição de rolagem
+var navbar = document.getElementById("navbar-total");
+var threshold = 90; // valor temporário, mas provavelmente ok
+
+/* Descobre até onde devemos 'segurar' a navbar.*/
+window.onload = function() {
+  threshold = navbar.offsetHeight; // valor errado antes de carregar a página, então precisa ser aqui
+}
+
+/*Quando o usuário rola para baixo, abaixa gradualmente a navbar. 
+Quando rola para cima, mostra a navbar. */
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset; // compara as posições de roalgem anterior e atual
+  /* compara as posições de rolagem anterior e atual*/
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    navbar.style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-21%"; // joga pra cima da tela a navbar
+    /*if (currentScrollPos < threshold) {
+      document.getElementById("navbar").style.top = "-7%"; // joga um pouco pra cima a navbar
+      //document.getElementById() diminui logo tecs
+    } else {
+      document.getElementById("navbar").style.top = "-21%"; // joga completamente pra cima da tela a navbar
+    }*/
+    navbar.style.top = (-Math.min(threshold, window.scrollY) ) + 'px'; // esconde progressivamente a navbar
   }
   prevScrollpos = currentScrollPos;
 };
