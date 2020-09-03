d8869d7fd58fa18c58de1d59275da268ae7c549c
abeRC <aeciobeltrao@usp.br>
2020-09-03 08:27:38 -0300

    behead iterates over all files in the current folder

--- beheader.py
+++ beheader.py
@@ -2,12 +2,12 @@
 import os
 import re
 
-victim = "index.html"
+victim = "index.html" # Where to extarct the head from.
+folder = "." #Folder containing all the HTML files to have their head replaced.
 # Matches something that starts with <!DOCTYPE, ends with head> and that contains 
 # either whitespace or not whitespace in-between.
 regex = "<!DOCTYPE[\s\S]+head>"
 new_head = None
-
 
 def update_head (filename):
     with open(filename, "r+") as f:
@@ -16,12 +16,23 @@ def update_head (filename):
         f.write(new)
 
 def main ():
-    global new_head
-
+    # Extract head from victim.
     with open(victim, "r") as f:
         match = re.match(regex, f.read())
         new_head = match.group()
-    update_head("testb.html")
+    
+    # Replace head in all files
+    rootdir = os.path.join(os.path.curdir, folder)
+    # from: https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
+    for subdir, dirs, files in os.walk(rootdir): # maybe os.path.abspath(rootdir) ?
+        for file in files:
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".html"):
+                if victim not in filepath:
+                    print("Updating head in", filepath, "...")
+                    update_head(filepath)
+                    break
 
 if __name__ == "__main__":
     main()
