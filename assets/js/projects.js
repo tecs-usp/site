aee90c5ec7be4ed869040eb7d88beb6face110a3
abeRC <aeciobeltrao@usp.br>
2020-09-05 00:08:36 -0300

    basic slide in

---
+++ assets/js/projects.js
@@ -0,0 +1,18 @@
+var timeoutID;
+var elems = document.getElementsByClassName("card-thing-animated");
+
+for (var i = 0; i < elems.length; i++) {
+	var card = elems[i];
+	card.onmouseover = function(e) {
+		console.log(this);
+		var description = this.children[1]; // div after the img
+		description.style.right = "0";
+		description.style.opacity = "1";
+	}
+	card.onmouseout = function(e) {
+		var description = this.children[1];
+		description.style.right = "-210px";
+		description.style.opacity = "0";
+		//description.style.right = "-210px";
+	}
+}
