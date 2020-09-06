4703a5d97f22521e2f45a66869344dfbbc4f073d
abeRC <aeciobeltrao@usp.br>
2020-09-06 11:39:15 -0300

    script fallback

--- assets/js/img-http-fallback.js
+++ assets/js/img-http-fallback.js
@@ -1,3 +1,4 @@
+/** In case an image fails to load with https, tries to load it via bare HTTP.*/
 function httpFallback (image) {
   image.onerror = null; /* clear error*/
   image.src = image.src.replace("https://","http://"); /* fallback to http to hopefully fix the problem */
