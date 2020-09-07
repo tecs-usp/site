c46a9eefdcdfe7ea8435d9b6ef0e40ea4e21e7e1
abeRC <aeciobeltrao@usp.br>
2020-09-07 08:12:46 -0300

    cleaning up a bit more maybe

--- assets/js/script-fallback.js
+++ assets/js/script-fallback.js
@@ -4,6 +4,10 @@ var scriptList = [
 		integrity: "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
 	}, 
 	{
+		src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
+		integrity: "sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+	},
+	{
 		src: "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js", 
 		integrity: "sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
 	},
@@ -11,7 +15,6 @@ var scriptList = [
 
 /** Makes sure scripts are loaded in the correct order. */
 function loadScript (i) {
-	console.error("I AM "+i);
 		var newScript = document.createElement('script');
 		newScript.setAttribute("src", scriptList[i].src);
 		newScript.setAttribute("integrity", scriptList[i].integrity);
