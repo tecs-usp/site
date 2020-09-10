5ec32f6e6109441640d4accc94d5f376e9af4a8c
abeRC <aeciobeltrao@usp.br>
2020-09-09 22:40:33 -0300

    fixed a wrongfully dismissive check in beheader-file.py and cleaned it up a bit

--- tools/beheader-file.py
+++ tools/beheader-file.py
@@ -3,11 +3,10 @@ import sys
 import os
 import re
 
-victim = "index.html" # Where to extarct the head from.
+victim = "index.html" # Where to extract the head from.
 folder = "." #Folder containing all the HTML files to have their head replaced.
-# Matches something that starts with <!DOCTYPE, ends with head> and that contains 
+regex = "<!DOCTYPE[\s\S]+head>" # Matches something that starts with <!DOCTYPE, ends with head> and that contains
 # either whitespace or not whitespace in-between.
-regex = "<!DOCTYPE[\s\S]+head>"
 new_head = None
 
 def update_head (filename):
@@ -26,16 +25,15 @@ def behead (victim, folder):
         new_head = match.group()
     
     # Replace head in all files
-    rootdir = os.path.join(os.path.curdir, folder)
-    # from: https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
-    for subdir, dirs, files in os.walk(rootdir): # maybe os.path.abspath(rootdir) ?
+    rootdir = os.path.abspath(folder)
+    # from: https://stackoverflow.com/a/30255302
+    for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             filepath = subdir + os.sep + file
 
-            if filepath.endswith(".html"):
-                if victim not in filepath:
-                    print("Updating head in", filepath, "...")
-                    update_head(filepath)
+            if filepath.endswith(".html") and not os.path.samefile(victim, filepath):
+                print("Updating head in", filepath, "...")
+                update_head(filepath)
 
 def print_help():
     message = """Helper script to extract the head from a specific HTML file and replicate it
