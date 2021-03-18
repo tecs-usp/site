ecf19fde7f37110c6b9a92e540492fca7940cdba
abeRC <aeciobeltrao@usp.br>
2021-03-18 19:08:08 -0300

    Alt text changes; div with figcaption -> figure
    Former-commit-id: 4bb640433af1b7680426f2334541bdd86ecc0940

--- oi/index.php
+++ oi/index.php
@@ -23,7 +23,7 @@
   <!-- Conteúdo em si -->
   <section id="textos" class="container-fluid explicacoes">
     <h3>O Tecs, como um todo</h3>
-    <p class="paragr4fo">O Tecs foi fundado em setembro de 2017 com o objetivo de fomentar maiores debates e ações dentro e fora da comunidade uspiana sobre a tecnologia, a sociedade e a relação entre elas. Como atividade inaugural do grupo, o Tecs organizou a Semana de Computação Social> no IME.</p>
+    <p class="paragr4fo">O Tecs foi fundado em setembro de 2017 com o objetivo de fomentar maiores debates e ações dentro e fora da comunidade uspiana sobre a tecnologia, a sociedade e a relação entre elas. Como atividade inaugural do grupo, o Tecs organizou a Semana de Computação Social no IME.</p>
     <p>Conforme a Constituição Federal define, as universidades têm <span class="marca-texto">três pilares: ensino, pesquisa e extensão; nós nos encaixamos no pilar da extensão</span>.</p>
       
     <p>Sendo assim, o Tecs é um grupo de extensão que desenvolve projetos focados no impacto social da tecnologia e que é formado majoritariamente por estudantes da USP.</p>
@@ -41,12 +41,15 @@
         <p>A Fundação CASA (Centro de Atendimento Socioeducativo ao Adolescente) é uma instituição estadual que aplica medidas socioeducativas segundo o Estatuto da Criança e do Adolescente (ECA) e o Sistema Nacional de Atendimento Socioeducativo (SINASE).</p>
         <p>Mas, mesmo inseridos em medidas de privação de liberdade, esses adolescentes devem ter acesso a educação escolar, cultura, esportes e lazer. E é aí que entra o curso de Introdução à Programação.</p>
         <p>Desta forma, o curso visa contribuir para o desenvolvimento pessoal e profissional de jovens que cumprem medidas socioeducativas, auxiliando-os no desenvolvimento de competências que ampliem suas possibilidades de inserção no mercado de trabalho, geração de renda e participação na sociedade. Sempre permeado por uma abordagem humanista que dialogue com as vivências individuais de cada aluno, o curso não se restringe ao ensino da técnica pela técnica e põe em destaque a interação e o desenvolvimento de interesses pessoais.</p>
-        <div class="col-md-8 imagem">
+        <figure class="col-md-8 imagem">
           <img src="https://tecs.ime.usp.br/assets/img/FCASA/2019-2.jpg" alt="6 pessoas de pé na frente do portão de uma unidade da Fundação CASA.">
-          <figcaption>Equipe do GT na frente de uma unidade da fundação CASA</figcaption>
-        </div>
+          <!--Legenda é redundante para quem está usando um leitor de tela e já ouviu o texto alternativo.
+          aria-hidden="true" esconde esse elemento para leitores de tela.
+          Na verdade, talvez não devesse ser redundante, mas não vejo como deixar diferente o bastante para jsutificar não esconder.-->
+          <figcaption aria-hidden="true">Equipe do GT na frente de uma unidade da fundação CASA</figcaption>
+          </figure>
         <p>Neste 4º oferecimento, a proposta do curso é desenvolver um jogo usando o Processing (modo Python), ferramenta que facilita a criação de programas visuais e expressivos. Isto será realizado gradativamente ao decorrer das aulas com o auxílio de um professor e de monitores que atuam de forma mais pessoal e direta ao esclarecer dúvidas e verificar o entendimento de cada aula.</p>
-      </div>
+        </div>
 
       <div class="col-md-4 explicacoes gt">
         <h6 class="como-ajudar">Como você pode ajudar?</h6>
@@ -62,10 +65,10 @@
         <p>O projeto, em parceria com a Rede Indígena da USP, tem como objetivo desenvolver um site centralizador de outros sites de aldeias indígenas de diferentes etnias. Com isso, ajudamos a melhorar a situação da visibilidade indígena através da inserção das aldeias no ambiente virtual.</p>
         <p>O Portal Indígena é, portanto, uma página pensada para ter um pouco da cara de todas as aldeias com as quais trabalhamos, permitindo divulgação de conteúdo cultural sobre cada uma delas e, principalmente, levantando discussões sobre questões sociais e territoriais.</p>
         <p>Estamos na fase de programação, focando nossa energia no desenvolvimento web da página já arquitetada num protótipo visual e aprovada pela própria Rede em reunião.</p>
-        <div class="col-md-8 imagem">
-          <img src="https://tecs.ime.usp.br/assets/img/PI/printPortal.png" alt="Captura da página inicial do portal indígena, com uma barra de navegação e imagens temporárias repetidas.">
-          <figcaption>Print da homepage do portal</figcaption>
-        </div>
+        <figure class="col-md-8 imagem">
+          <img src="https://tecs.ime.usp.br/assets/img/PI/printPortal.png" alt="Captura da página inicial do Portal Indígena, com uma barra de navegação e imagens temporárias repetidas.">
+          <figcaption aria-hidden="true">Print da homepage do portal</figcaption>
+          </figure>
         <p>Uma vez finalizada esta fase, o Tecs e a Rede devem voltar a alinhar suas expectativas para que possamos partir para o próximo ciclo, com a adição de ferramentas ao Portal como: calendário de plantio, dicionário Português-Guarani e mercado de artesanato indígena; são alguns dos primeiros aparatos adicionais ao site requisitados pela Rede.</p>
         <p>Além da interessante parte técnica associada, o contato com essa população tão apagada e abandonada pelo poder público é bastante enriquecedor em ambas as direções :)</p>
         <p>Novamente, é bastante importante ter em mente que não é necessário nenhum conhecimento prévio em programação para ajudar no projeto, uma vez que temos membros absolutamente dispostos e pacientes para ensinar tudo que sabem e, posteriormente, desenvolver em conjunto com os novos participantes!</p>
@@ -85,10 +88,10 @@
         <p>O projeto foi idealizado em 2019. Ele nasce não só da necessidade de incentivar mais mulheres na carreira de computação, mas também um perfil específico de mulheres: as de baixa renda.</p>
         <p>Entretanto, como muitas escolas públicas não têm salas de computadores, temos dificuldades de levar o curso para as escolas. Mas, como várias meninas já vem à USP para ter aulas nos cursinhos populares, percebemos que, para uma primeira iniciativa, não precisávamos ir até os bairros para atingi-las.</p>
         <p>Assim nasce o Vestibulandas: o projeto do Tecs que visa dar aulas de programação em Python para as alunas dos cursinhos populares da USP utilizando a própria estrutura da Universidade. Python foi a linguagem escolhida não só por ser a de maior parte dos integrantes no momento, como também a linguagem mais usada no mundo nos cursos de introdução.</p>
-        <div class="col-md-8 imagem">
+        <figure class="col-md-8 imagem">
           <img src="https://tecs.ime.usp.br/assets/img/Vestibulandas/PIPA/IMG_20200304_133648.jpg" alt="Duas pessoas segurando o troféu do PIPA (em material laminado, uma pessoa empinando pipa) e vestindo camisetas com o logo do grupo.">
-          <figcaption>Vitor e Ju segurando o prêmio PIPA.</figcaption>
-        </div>
+          <figcaption aria-hidden="true">Vitor e Ju segurando o prêmio PIPA.</figcaption>
+        </figure>
         <p>O projeto tem duas fases. A primeira: de organização - contato com os cursinhos e manejo da agenda - e planejamento - definição do modelo do curso, revisão e adaptação de materiais para ensino de programação, incluindo a triagem de exercícios de vestibulares que podem ser resolvidos com programação.</p>
         <p>E a segunda: as aulas. Para as aulas, pretendemos contar com uma equipe de monitoras proporcional ao números de alunas. Também nos parece interessante a equipe ser o mais interdisciplinar possível pra mostrar a variedade de aplicações e possibilidades que as ferramentas que elas estarão aprendendo tem; na esperança, também, das profissionais as inspirarem ao exporem seus diversos trabalhos.</p>
       </div>
@@ -113,10 +116,10 @@
           <li>receber, encaminhar e responder propostas de novos projetos;</li>
           <li>administrar as redes sociais;</li>
         </ul>
-        <div class="col-md-8 imagem">
+        <figure class="col-md-8 imagem">
           <img src="https://tecs.ime.usp.br/assets/img/Promos/site-novo.png" alt="Imagem com os dizeres: 'O site está de design novo!' e 'tecs.ime.usp.br'.">
-          <figcaption>Arte do MarkeTecs anunciando o novo design do site</figcaption>
-        </div>
+          <figcaption aria-hidden="true">Arte do MarkeTecs anunciando o novo design do site</figcaption>
+        </figure>
         <p>Temos basicamente dois subGTs aqui:</p>
         <ul style="padding-left: 1rem; list-style-type: circle;">
           <li>MarkeTecs: cuida das artes gráficas e materiais de divulgação externa;</li>
@@ -138,10 +141,10 @@
     <div class="row">
       <div class="col-md-8 explicacoes">
         <p>A democratização do uso da computação aliada à diversidade em seu interior é de extrema importância ao Tecs. Sendo essa uma área ainda repleta de injustiças para os mais diversos grupos sociais, as particularidades de pessoas LGBT's na computação é tema de fundamental relevância para um grupo que lida com o uso da tecnologia  como ferramenta de transformação social. É com essa motivação de promover a inclusão e diversidade nas áreas que envolvam computação que surge o grupo de trabalho mais jovem do Tecs: o LGBTecs.</p>
-        <div class="col-md-8 imagem">
-          <img src="https://tecs.ime.usp.br/assets/img/Logos/LGBTECS.png" alt="Logotipo do GT.">
-          <figcaption>Logotipo do GT</figcaption>
-        </div>
+        <figure class="col-md-8 imagem">
+          <img src="https://tecs.ime.usp.br/assets/img/Logos/LGBTECS.png" alt="'LGBTecs' sobre um gradiente arco-íris.">
+          <figcaption aria-hidden="true">Logotipo do GT</figcaption>
+        </figure>
         <p>Propomos o oferecimento de um curso de introdução à lógica de programação para pessoas trans em parceria com instituições voltadas à população LGBTQIA+. O Cursinho Transformação é um coletivo que acolhe pessoas trans em iniciativas ligadas desde ao oferecimento de um cursinho pré-vestibular até a defesa de seus direitos de sociabilização e acesso à educação, e é essa a instituição o nosso principal contato de interesse para o desenvolvimento do curso.</p>
         <p>Criado no início de agosto de 2020, o LGBTecs está em fase de estruturação e discussão de ideias voltadas à promoção da diversidade nas áreas de computação. Atualmente, o planejamento e estruturação do curso de introdução à lógica de programação para a população trans é a nossa principal atividade.</p>
       </div>
@@ -164,10 +167,10 @@
         <p>MAC0485 - Implicações Sociais da Computação é uma (outra) disciplina oferecida pelo IME sob coordenação do Tecs.</p>
         <p>O curso tem por objetivo apresentar, de maneira mais profunda, questões sociais, profissionais, legais, éticas, ambientais e políticas relacionadas à ciência da computação, além de possibilitar a compreensão do impacto da computação e suas tecnologias na sociedade no que concerne ao atendimento e à antecipação estratégica das necessidades da sociedade.</p>
         
-        <div class="col-md-8 imagem">
-          <img src="https://tecs.ime.usp.br/assets/img/Disciplinas/IMG_20191029_133103.jpg" alt="Palestrante Rafael palestrando.">
-          <figcaption>Prof° Zanatta palestrando em D&S</figcaption>
-        </div>
+        <figure class="col-md-8 imagem">
+          <img src="https://tecs.ime.usp.br/assets/img/Disciplinas/IMG_20191029_133103.jpg" alt="Professor voltado para o canto esquerdo da sala, de frente para estudantes que fazem anotações em cadernos e laptops. Ao fundo, uma larga lousa de giz com esquemas explicativos.">
+          <figcaption aria-hidden="true">Prof° Rafael palestrando em D&S</figcaption>
+        </figure>
       </div>
       
       
