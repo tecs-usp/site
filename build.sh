9db7f0014afbca8b7d887c5cf3da5873a33a23c9
abeRC <aeciobeltrao@usp.br>
2021-03-21 10:41:30 -0300

    Changed regex script so it doesn't mess up the PHP files.

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ mkdir -p build
 
 # Copy all project files to the build folder
 shopt -s extglob # enable extglob
-pattern_match=!(build|*.sh|LICENSE|README.md) # http://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Pattern-Matching
+pattern_match=!(build|tools|LICENSE|README.md|*.sh|*.txt) # http://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Pattern-Matching
 cp -r ./$pattern_match -t build # copy all files in the current directory (except those in $pattern_match) to build/
 
 # rsync solution
 # rsync -av --progress . build --exclude=PATTERN
 
-python tools/beheader-string.py build -m absolute-paths
+python tools/beheader-string.py build -m production-paths
