a55551d0d857d87cfc8517f2d68579f75bc175bc
abeRC <aeciobeltrao@usp.br>
2021-03-21 12:14:40 -0300

    Cleaning up CSS rules, alt text and scripting.
    Former-commit-id: 3cc7132f29f3b3e7470588694c23283383db7e58

--- assets/js/navbar-autohide.js
+++ assets/js/navbar-autohide.js
@@ -1,6 +1,7 @@
 /**
  * Quando o usuário rola para baixo, esconde a navbar. Quando rola para cima, mostra a navbar.
  * FIXME: https://developer.mozilla.org/docs/Mozilla/Performance/ScrollLinkedEffects
+ * Basically: this is really janky.
  * */
 
 var prevScrollpos = window.pageYOffset; // inicializa a posição de rolagem
@@ -10,26 +11,26 @@ var threshold = 90; // valor temporário, mas provavelmente ok
 /* Descobre até onde devemos 'segurar' a navbar.*/
 window.onload = function() {
   threshold = navbar.offsetHeight; // valor errado antes de carregar a página, então precisa ser aqui
-}
-
-/*Quando o usuário rola para baixo, abaixa gradualmente a navbar. 
-Quando rola para cima, mostra a navbar. */
-window.onscroll = function() {
-  /* compara as posições de rolagem anterior e atual*/
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    navbar.style.top = "0";
-  } else {
-    /*if (currentScrollPos < threshold) {
-      document.getElementById("navbar").style.top = "-7%"; // joga um pouco pra cima a navbar
-      //document.getElementById() diminui logo tecs
+  
+  /* Quando o usuário rola para baixo, abaixa gradualmente a navbar. 
+   * Quando rola para cima, mostra a navbar. */
+  window.onscroll = function() {
+    /* compara as posições de rolagem anterior e atual*/
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = "0";
     } else {
-      document.getElementById("navbar").style.top = "-21%"; // joga completamente pra cima da tela a navbar
-    }*/
-    navbar.style.top = (-Math.min(threshold, window.scrollY) ) + 'px'; // esconde progressivamente a navbar
-    $('.collapse').collapse("hide"); // collapse menu on scroll down
-  }
-  prevScrollpos = currentScrollPos;
-};
+      /*if (currentScrollPos < threshold) {
+        document.getElementById("navbar").style.top = "-7%"; // joga um pouco pra cima a navbar
+        //document.getElementById() diminui logo tecs
+      } else {
+        document.getElementById("navbar").style.top = "-21%"; // joga completamente pra cima da tela a navbar
+      }*/
+      navbar.style.top = (-Math.min(threshold, window.scrollY) ) + 'px'; // esconde progressivamente a navbar
+      $('.collapse').collapse("hide"); // for mobile devices, collapse menu on scroll down
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
 
 Array.prototype.map.call(document.getElementsByClassName("nav-link"), (item) => (item.innerHTML == "Contato") && (Math.random() > 0.99) ? (item.innerHTML = "ContaTecs") : "");
