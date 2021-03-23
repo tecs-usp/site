d5cb2d641dc8b4c93ba818372fd4fae411203c13
abeRC <aeciobeltrao@usp.br>
2021-03-23 02:44:02 -0300

    Renamed stuff and now I'm like, reaaaaaally sure that there's no clipping between navbar elements in any reasonable screen width.

---
+++ quem-somos/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt">
+  
+<head>
+  <?php require '../assets/components/head.html'; ?>
+
+  <title>TECS | Quem somos</title>
+</head>
+
+<body>
+  <?php require '../assets/components/navbar.html'; ?>
+
+  <div class="text-thing container">
+    <h2>Quem somos</h2>
+    <p class="explicacaoQS">
+      O 
+      <!-- colorful text start -->
+      <span class="text-thing-tecs-1">Tecs |</span> 
+      <span class="text-thing-tecs-2">Grupo de comput</span>
+      <span class="text-thing-tecs-1">{ação social}</span> 
+      <span class="text-thing-tecs-2">da USP</span> 
+      <!-- colorful text andy -->
+      é um grupo de extensão universitária 
+      iniciado por alunos do Bacharelado em Ciência da Computação do 
+      <a href="https://www.ime.usp.br" target="_blank" rel="noopener">IME-USP</a>,
+      focado no impacto social da computação e da tecnologia.
+      Ele é parte do <a href="https://techshift.org" target="_blank" rel="noopener">TechShift</a>,
+      uma aliança global de organizações estudantis com esse mesmo propósito.<br>
+    </p>
+    <p class="explicacaoQS">O grupo é constituído por três frentes de desenvolvimento — educação, ética e serviços. Elas visam, respectivamente, promover a educação tecnológica igualitária da população por meio de cursos, oficinas e ações promovidas pelo grupo; unir
+      esforços para formar uma sociedade e profissionais éticos e conscientes sobre o uso da 
+      tecnologia; e estimular alunos a usarem a tecnologia para solucionar problemas da 
+      comunidade local.
+    </p>
+   
+    <h2>Nossa missão</h2>
+    <p class="explicacaoQS">Usar a computação para fortalecer o pilar da extensão na universidade, promovendo uma 
+      relação dinâmica e bidirecionada de contato, suporte e aprendizado entre a comunidade 
+      universitária e a sociedade em que essa está inserida.
+    </p>
+    
+    <h2>Nossa visão</h2>
+    <p class="explicacaoQS">Um cenário de equidade dos saberes, no qual as universidades superem as barreiras de restrição de conhecimentos e técnicas. Pretendemos, a longo prazo, contribuir para uma formação universitária que estimule maior consciência social, capaz
+      de gerar profissionais da área de computação hábeis em refletir sobre as implicações éticas e sociais do seu trabalho, desmentindo, assim, o mito da neutralidade tecnológica. Ademais, colaborar para o melhor entendimento por parte da população
+      sobre o potencial de transformação da tecnologia, os seus usos na dinâmica social e as formas como ela pode ser usada a seu favor.
+    </p>
+    
+    <h2>Nossas atividades</h2>
+    <p class="explicacaoQS">
+      Encorajamos estudantes a encontrarem modos de combinar seu interesse pela ciência da computação e pelo impacto social.
+      Fazemos isso ao:
+    </p>
+    <ul>
+      <li>construir uma rede de estudantes interessados em melhorar a comunidade local e a sociedade, e criar oportunidades para que eles possam engajar-se em projetos com esse fim;</li>
+      <li>unir esforços para formar uma sociedade e profissionais éticos e conscientes sobre o uso da tecnologia;</li>
+      <li>promover a educação tecnológica igualitária da população por meio de cursos, oficinas e ações promovidas pelo grupo;</li>
+      <li>estimular alunos a usarem a tecnologia para solucionar problemas da comunidade local;</li>
+    </ul>
+    <p class="explicacaoQS">
+      Em termos gerais, pretendemos que os estudantes entendam como a tecnologia pode ser utilizada para o bem coletivo, e utilizem esse conhecimento na prática, por meio de colaborações com a comunidade local, os serviços públicos, as organizações
+      não-governamentais e as sem fins econômicos.
+    </p>
+  </div>
+
+  <?php require '../assets/components/footer.html'; ?>
+</body>
+</html>
