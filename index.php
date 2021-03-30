1011381da67630019e2548f28190900794c3cd55
abeRC <aeciobeltrao@usp.br>
2021-03-30 00:07:26 -0300

    Updated old/broken links and alt texts on main page.
    Former-commit-id: 8ea469efb9c3c470a01bbd9a403184e5e772e3eb

--- index.php
+++ index.php
@@ -12,36 +12,38 @@
 
   <div class="container-fluid">
 
-    <!--CAROUSEL START-->
+    <!-- CAROUSEL START -->
     <div class="slide-painel">
       <div class="carousel slide slide-painel" data-ride="carousel" id="carousel-1">
         <div class="carousel-inner" role="listbox">
+        
           <div class="active slide-painel carousel-item" role="option">
-            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/2017-12-07 00.14.01.jpg"
-              alt="Em um auditório do IME-USP, a 1ª geração do Tecs e palestrantes da Semana de Computação Social. No total, são 17 pessoas (dispostas em 3 fileiras) em frente de uma apresentação de eslaides por cima de uma lousa.">
-          </div>
-          <div class="slide-painel carousel-item" role="option">
-            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/IMG_4733.JPG"
-              alt='5 integrantes do Tecs (vestindo a camisa do grupo) em um auditório do IME-USP brincando com extensões elétricas, em um trocadilho com extensão universitária'>
-          </div>
-          <div class="slide-painel carousel-item" role="option">
-            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/encontecs-i-grupo.jpg"
-              alt="Em 2 fileiras, integrantes do Tecs (vestindo a camiseta do grupo) e participantes do EnconTecs em um auditório do IME-USP. Ao total, 18 pessoas na frente de uma tela de projetor.">
+            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/print1.png"
+              alt="Captura de tela de uma vídeo-chamada com 12 pessoas, cujas imagens formam um retângulo 4 por 3.">
           </div>
           <div class="slide-painel carousel-item" role="option">
             <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/2DSC05427.jpg"
-              alt="6 pessoas reunidas em volta de um integrante do Tecs (com a camiseta do grupo) apresentando o grupo em um evento de recepção de bixes em uma sala do IME-USP.">
+              alt="Pequeno público reunido em volta de uma pessoa apresentando o Tecs em uma sala.">
           </div>
           <div class="slide-painel carousel-item" role="option">
-            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/print1.png"
-              alt="3ª geração do Tecs (2020) em uma reunião virtual. A imagem é uma captura de tela de uma vídeo-chamada com 12 pessoas cujos rostos estão dispostos em 3 linhas e 4 colunas.">
+            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/encontecs-i-grupo.jpg"
+              alt="Em 2 fileiras, integrantes do Tecs (vestindo a camiseta do grupo) e participantes de evento em um auditório.">
           </div>
           <div class="slide-painel carousel-item" role="option">
             <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/2DSC05448.jpg"
-              alt="Dois integrantes do Tecs (com a camiseta do grupo) apresentam o grupo para duas pessoas. Entre os dois grupos, há uma mesa e um 'banner' do Tecs.">
+              alt="2 integrantes do Tecs apresentam o grupo para um pequeno público. Ao lado, um 'banner' do Tecs.">
+          </div>
+          <div class="slide-painel carousel-item" role="option">
+            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/IMG_4733.JPG"
+              alt='5 integrantes do Tecs em um auditório brincando com extensões elétricas, em um trocadilho com extensão universitária.'>
+          </div>
+          <div class="slide-painel carousel-item" role="option">
+            <img class="w-100 d-block" src="https://tecs.ime.usp.br/assets/img/Carousel/2017-12-07 00.14.01.jpg"
+              alt="Em um auditório, 17 pessoas dispostas em 3 fileiras. No fundo, uma apresentação de eslaides.">
           </div>
         </div>
-        <!--Carousel controls-->
+        
+        <!-- Carousel controls -->
         <div>
           <a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon"></span>
@@ -54,9 +56,9 @@
         </div> <!--.carousel-inner-->
       </div> <!--.carousel.slide.slide-painel-->
     </div> <!--.slide-painel-->
-    <!--CAROUSEL END-->
+    <!-- CAROUSEL END -->
 
-    <!--RELEVÂNCIA--> 
+    <!-- RELEVÂNCIA --> 
     <div class="row pt-5 d-flex justify-content-center align-items-center">
       <div class="col-11 col-md-5 text-center">
         <h2 class="font-weight-bold">Um grupo com relevância</h2>
@@ -70,17 +72,17 @@
           rel="noopener">
           <img class="w-100"
             src="https://revistapesquisa.fapesp.br/wp-content/themes/revista-pesquisa-fapesp-tema/img/logo_revista.jpg"
-            alt="Logo da revista Pesquisa FAPESP (Fundação de Amparo à Pesquisa do Estado de São Paulo)"
+            alt="Logo com o texto 'Pesquisa Fapesp'."
             onerror="httpFallback(this)">
           Revista Pesquisa FAPESP
         </a>
       </div>
       <div class="col-9 col-md-2 artigos-tecs-item my-2 my-md-0">
         <a class="w-70"
-          href="https://jornal.usp.br/universidade/acoes-para-comunidade/estudantes-da-usp-darao-aulas-de-computacao-para-adolescentes/"
+          href="https://jornal.usp.br/universidade/tecs-o-grupo-da-usp-que-utiliza-a-tecnologia-para-causas-sociais/"
           target="_blank" rel="noopener">
           <img class="w-100" src="https://www.scs.usp.br/identidadevisual/wp-content/uploads/2013/08/usp-logo-png.png"
-            alt="Logo da Universidade de São Paulo (USP)" onerror="httpFallback(this)">
+            alt="Logo com o texto 'Usp' em caixa alta e alguma sobreposição entre as letras." onerror="httpFallback(this)">
           Jornal da USP
         </a>
       </div>
@@ -89,38 +91,22 @@
           href="https://www.fundacaocasa.sp.gov.br/View.aspx?title=casas-osasco-i-e-ii-têm-curso-de-programação-da-usp&amp;d=9282"
           target="_blank" rel="noopener">
           <img class="w-100" src="https://www.fundacaocasa.sp.gov.br/images/logo-site.png"
-            alt="Logo da Fundação Centro de Atendimento Socioeducativo ao Adolescente (CASA)"
+            alt="Logo da Fundação Casa: 'Casa' com a letra C aludindo a uma casa."
             onerror="httpFallback(this)">
           Fundação CASA
         </a>
       </div>
       <div class="col-9 col-md-2 artigos-tecs-item my-5 my-md-0">
-        <a class="w-30" href="https://www.ime.usp.br/images/arquivos/aconteceime/acontecenoime_edicao_47.pdf"
+        <a class="w-30" href="https://www.ime.usp.br/media/acontece/acontecenoime_edicao_47.pdf"
           target="_blank" rel="noopener">
           <img class="w-100" src="https://ccsl.ime.usp.br/files/unmanaged/logos/logoIME+Texto.png"
-            alt="Logo do Instituto de Matemática e Estatística da USP (IME-USP). No brasão, identifica-se Arquimedes."
+            alt="Brasão do Ime-Usp (em preto e branco) no qual se identifica Arquimedes."
             onerror="httpFallback(this)">
           Revista Acontece no IME
         </a>
       </div>
     </div>
-    <!-- <div class="container text-center d-md-flex d-xl-flex justify-content-md-center justify-content-xl-center" style="padding-top: 33px;padding-bottom: 23px;max-width: 100%;">
-          <div class="text-center intro">
-              <h2>Um grupo com relevância</h2>
-              <p style="padding-top: 17px;padding-bottom: 15px;color: rgb(75,87,99);">Mesmo jovem, o Tecs já desenvolveu projetos que impactaram as pessoas envolvidas. Alguns destes nossos projetos saíram na mídia. Confira!</p>
-              <div>
-                  <ul class="list-group">
-                      <li class="list-group-item"><a href="https://revistapesquisa.fapesp.br/2019/11/15/algoritmos-parciais/" target="_blank">Matéria da FAPESP</a></li>
-                      <li class="list-group-item"><a href="https://jornal.usp.br/universidade/acoes-para-comunidade/estudantes-da-usp-darao-aulas-de-computacao-para-adolescentes/" target="_blank">Matéria do JORNAL DA USP</a></li>
-                      <li
-                          class="list-group-item"><a href="http://www.fundacaocasa.sp.gov.br/View.aspx?title=casas-osasco-i-e-ii-têm-curso-de-programação-da-usp&amp;d=9282" target="_blank">Matéria da Fundação CASA</a></li>
-                          <li class="list-group-item"><a href="https://www.ime.usp.br/images/arquivos/aconteceime/acontecenoime_edicao_47.pdf" target="_blank">Matéria do jornal Acontece no IME</a></li>
-                  </ul>
-              </div>
-          </div>
-      </div> -->
-  </div>
-  <!--container-fluid-->
+  </div> <!-- .container-fluid -->
 
   <?php require 'assets/components/footer2.html'; ?>
 </body>
